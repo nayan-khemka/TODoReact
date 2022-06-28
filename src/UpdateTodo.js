d3c2959df49ef6ce85b9ef3109f13b0f4292cd09
nayan-khemka <nayankhemka9@gmail.com>
2022-06-28 11:21:59 +0530

    changes made

--- src/UpdateTodo.js
+++ src/UpdateTodo.js
@@ -1,52 +1,94 @@
-import React, {useState} from "react";
+import { Typography, Box, makeStyles, Grid, TextField, Button } from "@material-ui/core"
+import { deepPurple, green } from '@material-ui/core/colors';
+import { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import axios from "axios";
 
-const UpdateTdo=(id)=>{
-    console.log(id);
-    const client = axios.create({
-        baseURL: `http://localhost:3006/todos/${id.id}`,
-    });
-    let todo={}
-    client.get().then(function(response){
-        todo=response.data
-    });
-    console.log(todo);
-    const [title, setTitle] = useState(todo.title);
-	const [completed, setCom] = useState(Boolean(todo.completed));
+const useStyles = makeStyles({
+ headingColor: {
+  backgroundColor: deepPurple[400],
+  color: "white"
+ },
+ addStuColor: {
+  backgroundColor: green[400],
+  color: "white"
+ },
 
-    const UpTodos=(title,completed)=>{
-        console.log(id,title,completed)
-        client.put('',
-        {
-            title:title,
-            completed:completed
-        })
-   };
+});
 
-    const handleSubmit = (e) => {
-		// e.preventDefault();
-		UpTodos(title, completed);
-	};
+const UpdateTdo = () => {
+ const classes = useStyles();
+ const { id } = useParams();
+ const history = useHistory();
+ const [student, setStudent] = useState({
+  stuname: "",
+  email: ""
+ });
+ useEffect(() => {
+  async function getStudent() {
+   try {
+    const student = await axios.get(`http://localhost:3006/todos/${id}`)
+    // console.log(student.data);
+    setStudent(student.data);
+   } catch (error) {
+    console.log("Something is Wrong");
+   }
+  }
+  getStudent();
+ }, [id]);
 
-    return(
+ function onTextFieldChange(e) {
+  setStudent({
+   ...student,
+   [e.target.name]: e.target.value
+  })
+ }
 
-        <form onSubmit={handleSubmit}>
-            {console.log(todo)}
-                  <input type="text" className="form-control"
-                    value={title}
-                    placeholder="Task"
-                    onChange={(e) => setTitle(e.target.value)}>{title}</input>
-                Completed:  <input type="checkbox"
-                        name=""
-                        className="form-control"
-                        id=""
-                        cols="10"
-                        rows="8"
-                        value={completed}
-                        onChange={(e) => setCom(e.target.value)}>
-                      </input>
-                      <button type="submit">Save</button>
-                </form>
-    );
+ async function onFormSubmit(e) {
+  e.preventDefault()
+  try {
+   await axios.put(`http://localhost:3006/todos/${id}`, student)
+   history.push("/")
+  } catch (error) {
+   console.log("Something is Wrong");
+  }
+ }
+ function handleClick() {
+  history.push("/")
+ }
+ return (
+  <>
+   <Box textAlign="center" p={2} className={classes.headingColor} mb={2}>
+    <Typography variant="h2">React CRUD with API Call</Typography>
+   </Box>
+
+   <Grid container justify="center" spacing={4}>
+    <Grid item md={6} xs={12}>
+     <Box textAlign="center" p={2} className={classes.addStuColor} mb={2}>
+      <Typography variant="h4">Edit Student</Typography>
+     </Box>
+     <form>
+      <Grid container spacing={2}>
+       <Grid item xs={12} sm={6}>
+        <TextField autoComplete="id" name="id" variant="outlined" required fullWidth id="id" label="ID" autoFocus value={id} disabled />
+       </Grid>
+       <Grid item xs={12} sm={6}>
+        <TextField autoComplete="stuname" name="stuname" variant="outlined" required fullWidth id="stuname" label="Name" value={student.stuname} onChange={e => onTextFieldChange(e)} />
+       </Grid>
+       <Grid item xs={12}>
+        <TextField autoComplete="email" name="email" variant="outlined" required fullWidth id="email" label="Email Address" value={student.email} onChange={e => onTextFieldChange(e)} />
+       </Grid>
+      </Grid>
+      <Box m={3}>
+       <Button type="button" variant="contained" color="primary" fullWidth onClick={e => onFormSubmit(e)}> Update </Button>
+      </Box>
+     </form>
+     <Box m={3} textAlign="center">
+      <Button variant="contained" color="primary" onClick={handleClick}>Back to Home</Button>
+     </Box>
+    </Grid>
+   </Grid >
+  </>
+ )
 }
-export default UpdateTdo;
+export default UpdateTdo
